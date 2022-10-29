72819c920ea6c5fd26d7082a9ad11c3f80a08fab
RobinStraka <robinstraka@hotmail.com>
2022-10-29 11:38:09 -0500

    Content Added

---
+++ week13react/log-in/src/navbar.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <div>
+              <ul id="nav">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+        );
+    }
+}
